6938e667115c5f5ef139564199e873c21cea497e
VykPa <vykpan@outlook.com>
2023-03-06 19:44:06 +0300

    реализовала CRUD операции

--- src/test/java/com/github/vikaclass/tryra/UserEndpoints.java
+++ src/test/java/com/github/vikaclass/tryra/UserEndpoints.java
@@ -1,19 +1,40 @@
 package com.github.vikaclass.tryra;
 
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 
 public class UserEndpoints {
     public static Response createUser(String payload){
-        return null;
+        RestAssured.baseURI = Routes.BASE_URI;
+        return RestAssured
+                .given().contentType(ContentType.JSON).accept(ContentType.JSON).body(payload)
+                .when().post(Routes.POST_URI);
+
     }
     public static Response readUser(String userName){
-        return null;
+        RestAssured.baseURI = Routes.BASE_URI;
+        return RestAssured
+                .given().pathParam("username", userName)
+                .when().get(Routes.GET_PUT_DELETE_URI);
     }
     public  static Response updateUser(String userName, String payload){
-        return null;
+        RestAssured.baseURI = Routes.BASE_URI;
+        return RestAssured
+                .given()
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .pathParam("username",userName)
+                .body(payload)
+                .when()
+                .put(Routes.GET_PUT_DELETE_URI);
     }
     public static Response deleteUser(String userName){
-        return null;
+        RestAssured.baseURI = Routes.BASE_URI;
+        return RestAssured
+                .given().pathParam("username", userName)
+                .when().delete(Routes.GET_PUT_DELETE_URI);
+
     }
 
 }
