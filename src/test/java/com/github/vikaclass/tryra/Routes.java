1bc934f111c17e4985f818666df1f8820472d2b8
VykPa <vykpan@outlook.com>
2023-03-06 19:25:15 +0300

    подключен maven добавила RestAssured и другие библиотеки для тестирования API подготовила два класса для ссылок и класс для CRUD операций

---
+++ src/test/java/com/github/vikaclass/tryra/Routes.java
@@ -0,0 +1,7 @@
+package com.github.vikaclass.tryra;
+
+public class Routes {
+    public static final String BASE_URI = "https://petstore.swagger.io/v2";
+    public static final String POST_URI = "/user";
+    public static final  String GET_PUT_DELETE_URI = "/user/{username}";
+}
---
+++ src/test/java/com/github/vikaclass/tryra/UserEndpoints.java
@@ -0,0 +1,19 @@
+package com.github.vikaclass.tryra;
+
+import io.restassured.response.Response;
+
+public class UserEndpoints {
+    public static Response createUser(String payload){
+        return null;
+    }
+    public static Response readUser(String userName){
+        return null;
+    }
+    public  static Response updateUser(String userName, String payload){
+        return null;
+    }
+    public static Response deleteUser(String userName){
+        return null;
+    }
+
+}
